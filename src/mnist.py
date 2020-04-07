59e9892926986886cb3b64fd1bacf17834e0b6a0
Takaaki Fukai <deep.wiki@gmail.com>
2020-04-08 02:45:32 +0900

    Add network definition

--- src/mnist.py
+++ src/mnist.py
@@ -27,3 +27,68 @@ testloader = torch.utils.data.DataLoader(testset,
                                          batch_size=100,
                                          shuffle=True,
                                          num_workers=2)
+
+###############
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net (nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+
+        # 1 channel --> 32 channel, 3x3 kernel, 1 stride
+        # (3x3 kernel x 32 = 288 parameters)
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+
+        # 32 channel --> 64 channel, 3x3 kernel, 1 stride
+        # (3x3 kernel x 2 (=64/2) = 18 parameters)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+
+        # Dropout with p = 0.25
+        self.dropout1 = nn.Dropout2d(0.25)
+
+        # Dropout with p = 0.5
+        self.dropout2 = nn.Dropout2d(0.5)
+
+        # 9216 nodes --> 128 nodes
+        # (9216 x 128 = 1179648 parameters)
+        self.fc1 = nn.Linear(9216, 128)
+
+        # 128 nodes --> 10 nodes
+        # (128 x 10 = 1280 parameters)
+        self.fc2 = nn.Lienar(128, 10)
+
+    def forward(self, x):
+        # Input: 28x28 x 1 channel
+
+        # 28x28 --[3x3 kernel, 1 stride convolution]--> 26x26
+        # 1 channel --> 32 channel
+        x = self.conv1(x)
+        x = F.relu(x)
+
+        # 26x26--[3x3 kernel, 1 stride convolution]-->24x24
+        # 32 channel --> 64 channel
+        x = self.conv2(x)
+        x = F.relu(x)
+
+
+        # 24x24--[2x2 maxpooling]-->12x12
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+
+        # 24x24 x 64 channel --> 9216 nodes
+        x = torch.flatten(x, 1)
+
+        # 9216 nodes --> 128 nodes
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+
+        # 128 nodes --> 10 nodes
+        x = self.fc2(x)
+
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
